baa682a2fc002e9a6077c58f50d4343eaedbac01
techzsf <tech.zsf32@gmail.com>
2025-09-24 13:03:53 +0530

    Fix JSX syntax error in dashboard page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -173,7 +173,6 @@ export default function DashboardPage() {
     }
   };
 
-  // @ts-ignore
   return (
     <div className="min-h-screen bg-gray-50 py-8">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
